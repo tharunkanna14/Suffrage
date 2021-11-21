72795090c694c9b461399efcf253b81075e214ef
Tharun Kanna <66260870+tharunkanna14@users.noreply.github.com>
2021-11-21 08:29:12 +0530

    Update settings.py

--- Suffrage/settings.py
+++ Suffrage/settings.py
@@ -10,24 +10,8 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
 
-# To append EC2 public ipv4 to allowed hosts automatically
-import pycurl
-from io import BytesIO
-
-ALLOWED_HOSTS = []
-
-# Determine Public IP address of EC2 instance
-buffer = BytesIO()
-c = pycurl.Curl()
-c.setopt(c.URL, 'checkip.amazonaws.com')
-c.setopt(c.WRITEDATA, buffer)
-c.perform()
-c.close()
-# Body is a byte string, encoded. Decode it first.
-ALLOWED_HOSTS.append(buffer.getvalue().decode('iso-8859-1').strip())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -40,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j734e3_(1-@5etn795+e12%sc1))$k7*f9+@g*7sp&i%ibzwqw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
